a064fd405f9a72b633d3f1d225c18b323b720b3c
Chaitanya Chakka <f20190171@hyderabad.bits-pilani.ac.in>
2020-10-28 19:31:08 +0530

    The Project

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SignUpRedirect(View view){
+        Intent intent = new Intent(this, SigninPage.class);
+        startActivity(intent);
+    }
+}
