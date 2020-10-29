193ecf49b9578e7e96bf423651b52f70a6520101
K.S.V.L.Druthi <f20190023@hyderabad.bits-pilani.ac.in>
2020-10-30 01:54:37 +0530

    30/10 1:53

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -5,14 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register=findViewById(R.id.RegisterButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,  SigninPage.class));
+                finish();
+            }
+        });
     }
 
     public void SignUpRedirect(View view){
