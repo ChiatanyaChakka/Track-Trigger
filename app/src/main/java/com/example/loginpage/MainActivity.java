a6416d6f7e835265e52c9a2fab1594c655013f84
shash <f20190003@hyderabad.bits-pilani.ac.in>
2020-10-30 00:52:35 +0530

    30/10 12:50AM

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -5,17 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.UserEmail);
+        password = findViewById(R.id.editTextTextPassword);
+        login = findViewById(R.id.SubBut);
+
+        auth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                logIn(txt_email, txt_password);
+            }
+        });
     }
 
-    public void SignUpRedirect(View view){
+    private void logIn(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, SigninPage.class));
+                finish();
+            }
+        });
+    }
+
+    public void SignUpRedirect(View view) {
         Intent intent = new Intent(this, SigninPage.class);
         startActivity(intent);
     }
