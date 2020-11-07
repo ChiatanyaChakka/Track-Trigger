045fdc08f7511e24cda9ec7120cdee8b5739fff1
shashi <shashivardhan201@gmail.com>
2020-11-07 15:54:48 +0530

    Added toast for empty login credentials.

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.loginpage;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -55,7 +56,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String txt_email = email.getText().toString();
                 String txt_password = password.getText().toString();
-                logIn(txt_email, txt_password);
+                if(TextUtils.isEmpty(txt_email)||TextUtils.isEmpty(txt_password)){
+                    Toast.makeText(MainActivity.this, "Fill the required credentials!", Toast.LENGTH_SHORT).show();
+                }else {
+                    logIn(txt_email, txt_password);
+                }
             }
         });
     }
